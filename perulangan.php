a0d3da521dc356db9e10627d80e3c0f3ffe10996
RaiStillLearning <rakhaarkhana98@gmail.com>
2025-04-19 15:36:29 +0700

    perulangan

---
+++ perulangan.php
@@ -0,0 +1,14 @@
+<?php
+// jenis jenis perulangan dalam PHP adalah untuk mengulangi blok kode tertentu berdasarkan kondisi yang ditentukan. Berikut adalah beberapa contoh cara kerja perulangan dalam PHP:
+// // // 1. for loop : digunakan untuk mengulangi blok kode dengan jumlah iterasi yang ditentukan.
+// // // 2. while loop : digunakan untuk mengulangi blok kode selama kondisi tertentu terpenuhi.
+// // // 3. do-while loop : mirip dengan while loop, tetapi blok kode dieksekusi setidaknya sekali sebelum memeriksa kondisi.
+// // // 4. foreach loop : digunakan untuk mengulangi elemen dalam array atau objek.
+// // // 5. break : digunakan untuk keluar dari perulangan.
+
+
+
+for ($i = 0; $i <= 100; $i++) {
+    echo "Perulangan ke-$i <br>";
+}
+?>
