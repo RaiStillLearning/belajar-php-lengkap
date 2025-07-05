c8460597d05c7c2f59114eb561ba09c5f6d21e53
Xarch <rakhaarkhana98@gmail.com>
2025-07-05 13:55:53 +0700

    belajar ambil data json api

---
+++ bab15/json_api.php
@@ -0,0 +1,20 @@
+<?php
+// $json = file_get_contents('https://api.brianabraham.web.id/api/v1/books');
+// $data = json_decode($json, true);
+
+// if($data) {
+//     echo 'Nama: ' .$data['data'][0]['judul_buku'] . '<br>';
+// }
+// mengirim json api
+
+
+header('Content-Type: application/json; charset=utf-8');
+
+$response = [
+    'status' => 'success',
+    'message' => 'data berhasil diproses',
+
+];
+
+echo json_encode($response, JSON_PRETTY_PRINT);
+?>
