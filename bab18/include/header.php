3fc81d2c5711e590d3e9b8c1a307e8c307608ba6
Xarch <rakhaarkhana98@gmail.com>
2025-07-19 13:43:37 +0700

    route

---
+++ bab18/include/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>DUMET School</title>
+<link href="assets/style.css" rel="stylesheet" type="text/css"/>
+<link href="assets/shortcutdils.ico" rel="shortcut icon"/>
+</head>
+<body>
+<div class="wrapper">
+    
+    <header><!-- mulai header -->
+        <h1>DUMET School</h1>
+    </header><!-- akhir header -->
+
+   <menu><!-- mulai menu -->
+        <ul>
+            <li><a href="index.php?page=home">Home</a></li>
+            <li><a href="index.php?page=about">About Us</a></li>
+            <li><a href="index.php?page=product">Product</a></li>
+            <li><a href="index.php?page=contact">Contact Us</a></li>
+        </ul>
+    </menu><!-- akhir menu -->
